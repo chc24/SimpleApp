8d475cd6eecc4db18c4f41adc98d39e52b111418
Calvin Chueh <chc24@duke.edu>
2015-07-16 10:19:12 -0700

    first commit

--- SimpleApp/ViewController.swift
+++ SimpleApp/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +20,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    // Let's handle the button
+    @IBAction func buttonTapped(sender: AnyObject) {
+        println("Ouch")
+        label.text = "asdf"
+    }
+    
+    @IBAction func ifLabelChanges(sender: AnyObject ) {
+        if let newlabel = label {
+        println("Label has changed once")
+        label.text  = "stop changing me"
+        }
+        else{
+            label.text = "oh this is the first time"
+        }
+    }
 }
 
